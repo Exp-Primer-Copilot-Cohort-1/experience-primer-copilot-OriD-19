67031da49373d0f340121193714b9f0a62ddb9ea
Fernando José Fuentes Castillo <83895570+OriD-19@users.noreply.github.com>
2024-06-27 00:04:56 +0000

    Copilot second commit!

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+  var member = document.getElementById("member").value;
+  var member = member.toLowerCase();
+  if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else if (member == "james") {
+    window.location.href = "james.html";
+  } else {
+    alert("Sorry, we don't have that member in our database.");
+  }
+}
